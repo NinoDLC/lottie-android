1cb3d890cfc74cd8e65474ee4024c9b4f044cb9f
Nino <anthony@delcey.fr>
2024-11-21 13:27:40 +0100

    Incorrect isAtEnd

--- issue-repro-compose/src/main/kotlin/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
+++ issue-repro-compose/src/main/kotlin/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
@@ -1,15 +1,18 @@
 package com.airbnb.lottie.issues.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieAnimationState
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
+import java.util.concurrent.atomic.AtomicInteger
 
 class ComposeIssueReproActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +22,23 @@ class ComposeIssueReproActivity : AppCompatActivity() {
         }
     }
 
+    private val toastCount = AtomicInteger()
+
     @Composable
     fun Content() {
         val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.heart))
-        val progress by animateLottieCompositionAsState(composition, iterations = LottieConstants.IterateForever)
-        LottieAnimation(composition, { progress })
+        val lottieAnimationState: LottieAnimationState = animateLottieCompositionAsState(composition)
+
+        if (lottieAnimationState.isAtEnd) {
+            Log.w("Bug", "isAtEnd ${toastCount.getAndIncrement()}")
+        }
+
+        LaunchedEffect(lottieAnimationState.isAtEnd) {
+            if (lottieAnimationState.isAtEnd) {
+                Log.w("Bug", "LaunchedEffect.isAtEnd ${toastCount.getAndIncrement()}")
+            }
+        }
+
+        LottieAnimation(composition)
     }
 }
